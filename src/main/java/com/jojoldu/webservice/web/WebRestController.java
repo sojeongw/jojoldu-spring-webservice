44f884f159632f32bf1230976fb8c993fb8e2e6f
sojeongw <redthink90@gmail.com>
2020-05-05 05:25:29 +0900

    2. SpringBoot & JPA로 간단 API 만들기 - controller, DTO 구현

--- src/main/java/com/jojoldu/webservice/web/WebRestController.java
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -1,13 +1,25 @@
 package com.jojoldu.webservice.web;
 
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // @ResponseBody를 모든 메소드에 적용해준다.
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;    // @Autowired 대신 생성자로 주입받는 것을 권장한다. 생성자는 @AllAlrgsConstructor가 만들어준다.
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
