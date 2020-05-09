dd4832ea1fd7e0dfbd6a27dc87e0595c970d21a0
sojeongw <redthink90@gmail.com>
2020-05-09 20:19:19 +0900

    3. SpringBoot & Handlebars로 화면 만들기

--- src/main/java/com/jojoldu/webservice/web/WebRestController.java
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -13,6 +13,11 @@ public class WebRestController {
 
     private PostsRepository postsRepository;    // @Autowired 대신 생성자로 주입받는 것을 권장한다. 생성자는 @AllAlrgsConstructor가 만들어준다.
 
+    @GetMapping("/")    // @GetMapping은 이전으로 보면 @RequestMapping(value="/", method = RequestMethod.GET)과 동일하다.
+    public String main() {
+        return "main";  // handlebars-spring-boot-starter 덕분에 컨트롤러에서 문자열을 반환할때 앞의 path와 뒤의 파일 확장자는 자동으로 지정된다. 즉, main.hbs가 로딩된다.
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
