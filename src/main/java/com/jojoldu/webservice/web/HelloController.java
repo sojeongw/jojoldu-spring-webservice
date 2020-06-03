eb92a0213fe704ba3868d207176152311f7b8761
sojeongw <redthink90@gmail.com>
2020-06-04 07:39:14 +0900

    2.4. HelloController 코드를 롬복으로 전환하기

--- src/main/java/com/jojoldu/webservice/web/HelloController.java
+++ src/main/java/com/jojoldu/webservice/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.webservice.web;
 
+import com.jojoldu.webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // JSON을 반환하는 컨트롤러로 만들어준다.
@@ -11,5 +13,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        /* @RequestParam
+        * 외부에서 API로 넘긴 파라미터를 가져오는 애너테이션이다.
+        * name이란 이름으로 넘긴 파라미터를 메서드 파라미터인 String name에 저장한다. */
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/jojoldu/webservice/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/webservice/web/HelloControllerTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /* 테스트를 진행할 때  JUnit에 내장된 실행자 외에 다른 실행자를 실행한다.
 * 여기서는 SpringRunner 라는 실행자를 사용한다.
@@ -40,5 +40,23 @@ public class HelloControllerTest {
                 // perform의 결과를 HTTP header의 status로 검증한다.
                 .andExpect(status().isOk()) // 200인지 아닌지 확인한다.
                 .andExpect(content().string(hello));    // 응답 본문의 내용을 검증한다.
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                // 요청 파라미터를 String만 허용하므로 숫자/날짜 등의 데이터도 String으로 변환한다.
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                // JSON 응답값을 필드별로 검증할 수 있는 메서드
+                // $를 기준으로 필드명을 명시한다. 즉, $.name으로 name을 검증한다.
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+
     }
 }
