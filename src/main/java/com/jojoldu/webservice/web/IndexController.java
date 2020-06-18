29f74f22fb314e71494407f60ac8bc118b2f6039
sojeongw <redthink90@gmail.com>
2020-06-18 22:52:19 +0900

    4.3. 게시글 등록 화면 만들기

--- src/main/java/com/jojoldu/webservice/web/IndexController.java
+++ src/main/java/com/jojoldu/webservice/web/IndexController.java
@@ -1,13 +1,44 @@
 package com.jojoldu.webservice.web;
 
+import com.jojoldu.webservice.service.posts.PostsService;
+import com.jojoldu.webservice.web.dto.PostsResponseDto;
+import com.jojoldu.webservice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
