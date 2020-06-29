bb40429ec14d6d9d3ff7a2f18e5e12e4120cdab4
sojeongw <redthink90@gmail.com>
2020-06-30 05:44:47 +0900

    5.2. 구글 로그인

---
+++ src/main/java/com/jojoldu/webservice/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,26 @@
+package com.jojoldu.webservice.config.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OAuthAttributes ofGoogle(String userNameAttributeName)
+}
