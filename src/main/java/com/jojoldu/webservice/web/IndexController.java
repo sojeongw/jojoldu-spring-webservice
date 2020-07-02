eb20689b82c0c29e2d471c283ca79ad885cb8aca
sojeongw <redthink90@gmail.com>
2020-07-02 12:13:36 +0900

    5.5. 세션 저장소로 데이터베이스 사용하기

--- src/main/java/com/jojoldu/webservice/web/IndexController.java
+++ src/main/java/com/jojoldu/webservice/web/IndexController.java
@@ -3,15 +3,10 @@ package com.jojoldu.webservice.web;
 import com.jojoldu.webservice.config.auth.LoginUser;
 import com.jojoldu.webservice.config.auth.dto.SessionUser;
 import com.jojoldu.webservice.service.posts.PostsService;
-import com.jojoldu.webservice.web.dto.PostsResponseDto;
-import com.jojoldu.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,23 +23,5 @@ public class IndexController {
         }
 
         return "index";
-    }
-
-    @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
-        return postsService.update(id, requestDto);
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
-
-        return "posts-update";
-    }
-
-    @GetMapping("/posts/save")
-    public String postsSave() {
-        return "posts-save";
     }
 }
